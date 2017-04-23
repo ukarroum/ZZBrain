7a18b32c756c5070f5d52e3033b160104ab8b59a
Imad E <enneiymy.i@gmail.com>
2017-04-23 06:11:00 +0200

    Multiplication function : working properly!

--- lib/matrix.cpp
+++ lib/matrix.cpp
@@ -4,10 +4,10 @@
 
 double * mult(double* M1, double* M2, int n, int m, int k){
     double mRes = new double[(n*k)]();
-    for (int i = 0; i < n; i+k) {
+    for (int i = 0; i < n; ++i) {
         for (int j = 0; j < k; ++j){
             for (int q = 0; q < m; ++q){
-                mRes[(i*m)+j]+=M1[i*m+q]*M2[(q*k)+j];
+                mRes[(i*k)+j]+=M1[i*m+q]*M2[(q*k)+j];
             }
         }
     }
