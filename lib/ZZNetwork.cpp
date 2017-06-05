bacf6a0a1f7565fbe2514b72a3bda8c10a465903
Yassir Karroum <ukarroum17@gmail.com>
2017-06-05 14:00:34 +0200

    Delete debugging statements

--- lib/ZZNetwork.cpp
+++ lib/ZZNetwork.cpp
@@ -72,11 +72,9 @@ double ***ZZNetwork::backPropagation(){
         for(int i = 0; i < setSize; i++) { // m = setSize //
             //Forward Propagation
             memcpy(a[0], input[i], network[0].nbNodesLayer1 * sizeof(double));
-            //cout << "a[0] : " << a[0][0] << " " << a[0][1] << endl;
             for (int l = 1; l < nbLayers; l++) {
                 memmove(a[l - 1] + 1, a[l - 1], network[l - 1].nbNodesLayer1 * sizeof(double));
                 a[l - 1][0] = 1;
-                //cout << "a[0] : " << a[0][0] << " " << a[0][1] << endl << endl;
                 z[l] = mult(network[l - 1].weights, a[l - 1], network[l - 1].nbNodesLayer2, network[l - 1].nbNodesLayer1 + 1, 1);
                 a[l] = sigmoid(z[l], network[l - 1].nbNodesLayer2, 1);
             }
@@ -104,15 +102,6 @@ double ***ZZNetwork::backPropagation(){
                 delete[] eltm1;
             }
 
-            if(i == 0)
-            {/*
-                cout << "err[2] : " << err[2][0] << endl;
-                cout << "err[1] : " << err[1][0] << " " << err[1][1] << " " << err[1][2] << endl;
-                cout << "a[2] : " << a[2][0] << " " << a[2][1] << endl;
-                cout << "a[1] : " << a[1][0] << " " << a[1][1] << " " << a[1][2] << endl;
-                cout << "a[0] : " << a[0][0] << " " << a[0][1] << " " << a[0][2] << endl;
-                cout << "input[i] : " << input[i][0] << " " << input[i][1] << endl;*/
-            }
 
             // Calculating bigDelta gradient accumelator;
 
@@ -122,17 +111,8 @@ double ***ZZNetwork::backPropagation(){
                 theta_delta = mult(err[l+1], tr, network[l].nbNodesLayer2, 1, network[l].nbNodesLayer1 + 1);
 
 				for(int k = 0; k < network[l].nbNodesLayer2; k++)
-                {
 					for(int j = 0; j < network[l].nbNodesLayer1 + 1; j++)
-                    {
-                        //cout << "lim : " << lim << endl;
-                        //cout << "k : " << k << endl;
-
                 		bigDelta[l][k][j] = bigDelta[l][k][j] + theta_delta[k * (network[l].nbNodesLayer1 + 1) + j];
-                        /*if(i == 0)
-                            cout << "bigDelta[" << l << "][" << k << "][" << j << "] = " << bigDelta[l][k][j] << endl;*/
-                    }
-                }
             }
 
         }
@@ -140,20 +120,10 @@ double ***ZZNetwork::backPropagation(){
        // Calculation D the gradient XD
 
        for(int l=0; l < nbLayers - 1; l++){
-          //int lim =  (l == nbLayers - 2)?network[l].nbNodesLayer2:(network[l].nbNodesLayer2 + 1);
           for(int i=0; i < network[l].nbNodesLayer2; i++) {
-                //cout << "bigDelta[" << l << "][" << i << "][" << 0 << "] = " << bigDelta[l][i][0] << endl;
                 D[l][i][0] = bigDelta[l][i][0]/setSize;
-                //cout << "D[" << l << "][" << i << "][" << 0 << "] = " << D[l][i][0] << endl << endl;
-                for(int j=1; j < network[l].nbNodesLayer1 + 1; j++){
-                    //cout << "bigDelta[" << l << "][" << i << "][" << j << "] = " << bigDelta[l][i][j] << endl;
-                    D[l][i][j] = bigDelta[l][i][j]/setSize + (lambda/setSize)*network[l].weights[i * (network[l].nbNodesLayer1 + 1) + j];
-                    //cout << "D[" << l << "][" << i << "][" << j << "] = " << D[l][i][j] << endl << endl;
-
-                }
-                // if j = 0
-
-
+                for(int j=1; j < network[l].nbNodesLayer1 + 1; j++)
+                    D[l][i][j] = bigDelta[l][i][j]/setSize + lambda*network[l].weights[i * (network[l].nbNodesLayer1 + 1) + j];
            }
        }
 
@@ -178,7 +148,6 @@ double *ZZNetwork::predict(double *input) {
     return a;
 }
 double ZZNetwork::costFunction(const column_vector& thetas){
-    //cout << "thetas : " << thetas << endl;
     int cpt = 0;
 
     for(int i = 0; i < nbLayers - 1; i++)
@@ -188,29 +157,10 @@ double ZZNetwork::costFunction(const column_vector& thetas){
 
     double sum = 0;
     double thetas_sum = 0;
-    for(int i = 0; i < setSize; i++){
-        for(int k = 0; k < network[nbLayers - 2].nbNodesLayer2; k++){
-          /*cout << "y : " << output[i][k] << endl;
-          			cout << "h(x) : " << predict(input[i])[k] << endl;*/
-                /*if( (output[i][k] == 1 && predict(input[i])[k] < 0.5) || output[i][k] == 0 && predict(input[i])[k] >= 0.5)
-                    sum += -1e+10;*/
-            //sum += output[i][k]*log(h) + (1 - output[i][k])*log(1 - h);
+    for(int i = 0; i < setSize; i++)
+        for(int k = 0; k < network[nbLayers - 2].nbNodesLayer2; k++)
             sum += output[i][k]*log(predict(input[i])[k]) + (1 - output[i][k])*log(1 - predict(input[i])[k]);
-            if(i == 0)
-            {
 
-                /*cout << "output[i][k] : "  << output[i][k] << endl;
-                cout << "input[i] : " << input[i][0] << " " << input[i][1] << endl;
-                cout << "predict(input[i])[k] : " << predict(input[i])[k] << endl;
-                cout << "1 - predict(input[i])[k] : " << 1 - predict(input[i])[k] << endl;
-                cout << "log(1 - predict(input[i])[k]) : " << log(1 - predict(input[i])[k]) << endl;
-                cout << "sum : " << sum << endl << endl;*/
-            }
-
-		}
-	}
-    /*if(sum > 0)
-        sum = -1e+10;*/
     sum /= -setSize;
 
     for(int i = 0; i < thetas.nr(); i++)
@@ -218,7 +168,6 @@ double ZZNetwork::costFunction(const column_vector& thetas){
 
     sum += (lambda/(2*setSize))*thetas_sum;
 
-    cout << "Cost functions : " << sum << endl;
     return sum;
 }
 const column_vector ZZNetwork::costFunctionDerivative(const column_vector& thetas)
@@ -239,8 +188,6 @@ const column_vector ZZNetwork::costFunctionDerivative(const column_vector& theta
 			for(int j = 0; j < network[l].nbNodesLayer1 + 1; j++, cpt++)
 				deriv(cpt) = D[l][i][j];
 
-
-    //cout << "D : " << deriv << endl;
 	return deriv;
 }
 
@@ -258,35 +205,9 @@ void ZZNetwork::train(){
 			for(int i = 0; i < network[l].nbNodesLayer1 + 1; i++, size++)
 				init_theta(size) = network[l].weights[j*(network[l].nbNodesLayer1 + 1) + i];
 
-    //init_theta = -5, -4, -3, -2, -1, 0, 1, 2, 3;
-    cout << "Inittial theta" << endl;
-    cout << init_theta << endl;
-    cout << "J : " << costFunction(init_theta) << endl;
-    cout << "D : " << costFunctionDerivative(init_theta) << endl;
-	find_min(bfgs_search_strategy(), objective_delta_stop_strategy(1e-7).be_verbose(),
+	find_min(bfgs_search_strategy(), objective_delta_stop_strategy(1e-7),
 		[this](column_vector const&v) -> double { return costFunction(v); },
 		[this](column_vector const&v) -> column_vector { return costFunctionDerivative(v); },
 		init_theta, -1);
-        /*find_min(bfgs_search_strategy(), gradient_norm_stop_strategy(1e-07).be_verbose(),
-    		[this](column_vector const&v) -> double { return costFunction(v); },
-    		[this](column_vector const&v) -> column_vector { return costFunctionDerivative(v); },
-    		init_theta, -1);*/
-        /*find_min_using_approximate_derivatives(bfgs_search_strategy(),
-           objective_delta_stop_strategy(1e-7),
-           [this](column_vector const&v) -> double { return costFunction(v); }, init_theta, -1); */
 
-    /******** Testing gradient descent *********/
-    /*init_theta = -0.189803, 0.0505138;
-    cout << "Inittial theta" << endl;
-    cout << init_theta << endl;
-    for(int i = 0; i < 50000; i++)
-    {
-        column_vector D = costFunctionDerivative(init_theta);
-        //cout << "D : " << D << endl;
-        init_theta = init_theta - 0.1*D;
-        //cout << "theta : " << init_theta << endl;
-    }*/
-    /*column_vector D = costFunctionDerivative(init_theta);*/
-	cout << "final theta : " << init_theta << endl;
-    /*cout << costFunction(init_theta) << endl;*/
 }
--- main.cpp
+++ main.cpp
@@ -18,34 +18,8 @@ int main() {
 	{
 		X[i] = new double[2];
 		Y[i] = new double[2];
-		/*
-		if(std::rand() % 2)
-		{
-			X[i][0] = 0.0;
-			Y[i][0] = 1.0;
-			xEq0++;
-		}
-		else
-		{
-			X[i][0] = 1.0;
-			Y[i][0] = 0.0;
-		}
-		*/
 	}
 
-
-	/*
-	for(int i = 0; i < 500; i++)
-	{
-		X[i][0] = 0.0;
-		Y[i][0] = 1.0;
-	}
-
-	for(int i = 500; i < 1000; i++)
-	{
-		X[i][0] = 1.0;
-		Y[i][0] = 0.0;
-	}*/
 
 	for(int i = 0; i < 250; i++)
 	{
@@ -85,47 +59,6 @@ int main() {
 	cout << "xnor(1, 1) = " << notNet.predict(X[900])[0] << endl;
 
 
-	/*
-	ZZNetwork notNet(sizes, nbLayers, 1000, X, Y, 0.001);
-	notNet.train();
-
-	cout << "not(0) = " << notNet.predict(X[0])[0] << endl;
-	cout << "not(1) = " << notNet.predict(X[900])[0] << endl;
-	*/
-
-/*
-	int sizes[] = {2, 1};
-	int nbLayers = 2;
-
-	double **X = new double*[4];
-	double **Y = new double*[4];
-
-	for(int i = 0; i < 4; i++)
-	{
-		X[i] = new double[2];
-		Y[i] = new double[1];
-	}
-
-	X[0][0] = 0.0;
-	X[0][1] = 1.0;
-	Y[0][0] = 0.0;
-
-	X[1][0] = 1.0;
-	X[1][1] = 0.0;
-	Y[1][0] = 0.0;
-
-	X[2][0] = 0.0;
-	X[2][1] = 0.0;
-	Y[2][0] = 0.0;
-
-	X[3][0] = 1.0;
-	X[3][1] = 1.0;
-	Y[3][0] = 1.0;
-
-	ZZNetwork andNet(sizes, nbLayers, 4, X, Y, 0.01);
-	andNet.train();
-
-*/
     return 0;
 
 }
